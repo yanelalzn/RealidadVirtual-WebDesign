da53cd7e792d45286a0d753c477a251aa8c8a879
Yanela <yanelalzn05@gmail.com>
2025-09-30 01:25:32 -0500

    mejoras de la Página Web

--- js/main.js
+++ js/main.js
@@ -21,3 +21,32 @@ window.addEventListener('scroll', ()=> {
         header.style.backgroundColor = 'transparent'
     }
 })
+
+/*============= SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 1000,
+  delay: 400,
+  easing: "ease-out",
+  // reset: true,
+});
+
+sr.reveal(`.banner`);
+
+sr.reveal(`#Header`, { delay: 400, origin: "top" });
+
+sr.reveal(`.title-second`, { delay: 600, origin: "bottom" });
+sr.reveal(`.redes-sociales`, { delay: 600, origin: "right" });
+
+sr.reveal(`.title-first`, { delay: 900, distance: 0});
+
+sr.reveal(`.title-third`, { delay: 1300, distance: 0, opacity: 0, origin: "bottom" });
+
+sr.reveal(`.boton-contacto`, { delay: 1100, distance: 0, scale: 0.5 });
+
+ScrollReveal().reveal('.boton-aventuras', {  delay: 1000, distance: '0px', scale: 0.5, opacity: 0, duration: 1000, easing: 'ease-in-out' });
+
+
+ScrollReveal().reveal('.banner', {  delay: 600, distance: '0px', opacity: 0, duration: 1000, easing: 'ease-in-out' });
